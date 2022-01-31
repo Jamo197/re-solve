8f9669812cc37365a54847db865285fd6646e43b
Jannes <jlampe97@web.de>
2022-01-31 15:11:05 +0100

    make it better

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -155,3 +156,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ) 
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
